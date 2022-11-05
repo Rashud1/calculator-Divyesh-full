4b27e7072dbcdf913453d62ac2975cc7244dfd65
Divyesh Shrestha <rashudshrestha12@gmail.com>
2022-11-05 12:22:52 +1100

    html and css of calculator completed

---
+++ app.js
@@ -0,0 +1,9 @@
+//selectors
+const display1E1 = document.querySelector('.display-1');
+const display2E2 = document.querySelector('.display-2');
+const temResultE1 = document.querySelector('.temp-result');
+const numbersE1= document.querySelectorAll ('.number');
+const operationE1= document.querySelectorAll ('.operation');
+const equalE1= document.querySelector ('.equal');
+const clearE1= document.querySelector ('.all-clear');
+const clearLastE1= document.querySelector ('.last-entity-clear');
